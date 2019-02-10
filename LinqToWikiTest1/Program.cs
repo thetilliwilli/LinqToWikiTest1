a55131cc354c22d1f407bfd070043c9400ba84b8
TilliWilli <tilliwilli@gmail.com>
2019-02-10 14:01:08 +0300

    +query cache

--- LinqToWikiTest1/Program.cs
+++ LinqToWikiTest1/Program.cs
@@ -17,25 +17,56 @@ namespace LinqToWikiTest1
         static void Main(string[] args)
         {
             //query
-            //var jsonResult = File.ReadAllText(@"query (3).json");
-            //var query = @"https://query.wikidata.org/sparql?format=json&query=SELECT%20%3Fvideo_game%20%3Fvideo_gameLabel%20%3Fpublisher%20%3FpublisherLabel%20%3Fpublication_date%20%3Fplatform%20%3FplatformLabel%20%3Fgenre%20%3FgenreLabel%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22.%20%7D%0A%20%20%3Fvideo_game%20wdt%3AP31%20wd%3AQ7889.%0A%20%20OPTIONAL%20%7B%20%3Fvideo_game%20wdt%3AP123%20%3Fpublisher.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fvideo_game%20wdt%3AP577%20%3Fpublication_date.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fvideo_game%20wdt%3AP400%20%3Fplatform.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fvideo_game%20wdt%3AP136%20%3Fgenre.%20%7D%0A%7D%0ALIMIT%2020";
-            var query = "SELECT ?video_game ?video_gameLabel ?publisher ?publisherLabel ?publication_date ?platform ?platformLabel ?genre ?genreLabel WHERE { SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". } ?video_game wdt:P31 wd:Q7889. OPTIONAL { ?video_game wdt:P123 ?publisher. } OPTIONAL { ?video_game wdt:P577 ?publication_date. } OPTIONAL { ?video_game wdt:P400 ?platform. } OPTIONAL { ?video_game wdt:P136 ?genre. } } LIMIT 20";
-            var jsonResult = GetResult(query);
-            var gamesDtoResult = JsonConvert.DeserializeObject<ResponseDto>(jsonResult);
-            var games = Program.ToGameInfos(gamesDtoResult);
+            var queryStartTime = DateTime.Now;
+                var query = "SELECT ?video_game ?video_gameLabel ?publisher ?publisherLabel ?publication_date ?platform ?platformLabel ?genre ?genreLabel WHERE { SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". } ?video_game wdt:P31 wd:Q7889. OPTIONAL { ?video_game wdt:P123 ?publisher. } OPTIONAL { ?video_game wdt:P577 ?publication_date. } OPTIONAL { ?video_game wdt:P400 ?platform. } OPTIONAL { ?video_game wdt:P136 ?genre. } }"
+                        + " LIMIT 9999"
+                        ;
+                var jsonResult = TryGetFromCache(query, ()=> GetResult(query));
+            var queryEndTime = DateTime.Now;
+
+            var preparingStartTime = DateTime.Now;
+                var gamesDtoResult = JsonConvert.DeserializeObject<ResponseDto>(jsonResult);
+                var games = Program.ToGameInfos(gamesDtoResult);
+            var preparingEndTime = DateTime.Now;
+
 
             //processing
-            var resultSet = games
-                    .Where(g => g.publication_date > DateTimeOffset.Parse("2000-01-01T00:00:00Z"))
-                    .Take(15)
-                    ;
+            var processingStartTime = DateTime.Now;
+                var resultSet = games
+                        .Where(g => g.publication_date > DateTimeOffset.Parse("2000-01-01T00:00:00Z") && g.publication_date < DateTimeOffset.Parse("2010-01-01T00:00:00Z"))
+                        .GroupBy(g => g.video_game)
+                        .Select(group => group.First())
+                        .Take(15)
+                        ;
+            var processingEndTime = DateTime.Now;
 
             //output
             resultSet
                 .ToList()
                 .ForEach(GameInfo.Print)
                 ;
+
+            WriteLine($"Timing: [Query: {(queryEndTime - queryStartTime).TotalMilliseconds}] [Porcessing: {(processingEndTime-processingStartTime).TotalMilliseconds}]");
             ReadKey();
+        }
+
+        private static string TryGetFromCache(string query, Func<string> retrieveRemoteContent)
+        {
+            var contentByteArray = System.Text.Encoding.Unicode.GetBytes(query);
+            using (var sha256 = System.Security.Cryptography.SHA256.Create())
+            {
+                var cacheKey = sha256.ComputeHash(contentByteArray);
+                //var cacheKeyBase64 = System.Convert.ToBase64String(cacheKey);
+                //var cacheKeyBase64 = System.Text.Encoding.ASCII.GetString(cacheKey);
+                //2.ToString()
+                var cacheKeyBase64 = new string(cacheKey.Select(@byte => @byte.ToString("X")[0]).ToArray());
+                if (File.Exists(cacheKeyBase64))
+                    return File.ReadAllText(cacheKeyBase64);
+
+                var newContent = retrieveRemoteContent();
+                File.WriteAllText(cacheKeyBase64, newContent);
+                return newContent;
+            }
         }
 
         static RestClient _client = PrepareClient();
@@ -55,7 +86,7 @@ namespace LinqToWikiTest1
             request.AddQueryParameter("format", "json");
             request.AddQueryParameter("query", query);
             //request.
-            WriteLine(_client.BuildUri(request));
+            //WriteLine(_client.BuildUri(request));
             var result = _client.Execute(request);
             if (result.ErrorException != null)
                 Environment.FailFast("Error while request", result.ErrorException);
@@ -76,7 +107,7 @@ namespace LinqToWikiTest1
                     publication_date = DateTimeOffset.Parse(binding[nameof(GameInfo.publication_date)].value),
                     platformLabel = binding[nameof(GameInfo.platformLabel)].value,
                     genreLabel = binding[nameof(GameInfo.genreLabel)].value,
-                    //video_game = binding[nameof(GameInfo.video_game)].value,
+                    video_game = binding[nameof(GameInfo.video_game)].value,
                     //publisher = binding[nameof(GameInfo.publisher)].value,
                     //platform = binding[nameof(GameInfo.platform)].value,
                     //genre = binding[nameof(GameInfo.genre)].value,
@@ -91,7 +122,7 @@ namespace LinqToWikiTest1
         public DateTimeOffset publication_date;
         public string platformLabel;
         public string genreLabel;
-        //public string video_game;
+        public string video_game;
         //public string publisher;
         //public string platform;
         //public string genre;
