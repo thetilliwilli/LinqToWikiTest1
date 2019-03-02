af4c6d4c6faab1714732b6183811e90d3a0c44d6
TilliWilli <tilliwilli@gmail.com>
2019-03-02 22:08:45 +0300

    QueryParser does minification of variable names

--- LinqToWikiTest1/PropertyRetriever/minify.js
+++ LinqToWikiTest1/PropertyRetriever/minify.js
@@ -1,11 +1,54 @@
-function* f1(){for(var i="a".charCodeAt(0);i<="z".charCodeAt(0);i++) yield String.fromCharCode(i);return;}
-function* f2(){for(var i="A".charCodeAt(0);i<="Z".charCodeAt(0);i++) yield String.fromCharCode(i);return;}
-var shorteningDic = [...f1(),...f2()];
+var originalLabelAppendix = "Label";
 
-//inputString
-inputString
-    .trim()
-    .slice(1)
-    .split(" ?")
-    .map((name,i)=>({name:name,short:shorteningDic[i]}))
-    ;
+function* azGenerator() { for (var i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++) yield String.fromCharCode(i); return; }
+function* AZGenerator() { for (var i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) yield String.fromCharCode(i); return; }
+function* azIndexedGenerator() {
+    for (var c = "a".charCodeAt(0); c <= "z".charCodeAt(0); c++)
+        for (var i = 0; i <= 9; i++)
+            yield String.fromCharCode(c) + i; //a0, a1, ... a9
+    return;
+}
+function* AZIndexedGenerator() {
+    for (var c = "A".charCodeAt(0); c <= "Z".charCodeAt(0); c++)
+        for (var i = 0; i <= 9; i++)
+            yield String.fromCharCode(c) + i; //a0, a1, ... a9
+    return;
+}
+
+function GenerateShorteningDictionary(n) {
+    var shorteningDic = [
+        ...azGenerator(), //cardinality 26
+        ...AZGenerator(), //cardinality 26
+        ...azIndexedGenerator(), //cardinality 260
+        ...AZIndexedGenerator(), //cardinality 260
+    ]; //total cardinality 572
+    if (shorteningDic.length < n)
+        throw new Error("Too many cardinality required");
+    return shorteningDic;
+}
+
+function GenerateShortener(names) {
+    names = names.sort((a, b) => a.localeCompare(b));
+    var shortDic = GenerateShorteningDictionary(names.length);
+    function Shortener(name) {
+        var withLabel = name.slice(-originalLabelAppendix.length) === originalLabelAppendix;
+        if (withLabel) {
+            var index = names.indexOf(name.slice(0, -originalLabelAppendix.length));
+            if (index === -1) throw new Error("Out of range");
+            return shortDic[index]+originalLabelAppendix;
+        }
+        else {
+            var index = names.indexOf(name);
+            if (index === -1) throw new Error("Out of range");
+            return shortDic[index];
+        }
+
+        
+    }
+    Shortener.shortDic = shortDic;
+    return Shortener;
+}
+
+module.exports = {
+    GenerateShortener,
+};
--- LinqToWikiTest1/PropertyRetriever/query-parser2.js
+++ LinqToWikiTest1/PropertyRetriever/query-parser2.js
@@ -1,32 +1,31 @@
-var bindingPrefix = "?";
-var labelAppendix = "Label";
-var languageServiceTag = "en";
+const minify = require("./minify");
 
-function RemoveDublicates(item, index, array){
+var resultLabelAppendix = "L";
+
+function RemoveDublicates(item, index, array) {
     return array.indexOf(item) === index;
 }
 
-function AddLabel(variable){
-    return variable + labelAppendix;
+function AddLabel(variable) {
+    return variable + resultLabelAppendix;
 }
 
-function AddLabelFilterClause(variable){
-    return `FILTER(LANG(?${variable}Label)="en")`
+function AddLabelFilterClause(variable) {
+    return `FILTER(LANG(${variable})="en")`
 }
 
-function AddVariableQuestionPrefix(variable){
-    return "?"+variable;
+function AddMark(variable) {
+    return "?" + variable;
 }
 
-function Sort(a,b){return a.localeCompare(b);}
+function Sort(a, b) { return a.localeCompare(b); }
 
-class QueryParser
-{
+class QueryParser {
     static Parse(query) {
 
         var trimmed = query.toLowerCase();
 
-        if(!trimmed.includes("select") || !trimmed.includes("where"))
+        if (!trimmed.includes("select") || !trimmed.includes("where"))
             throw new Error("Incorrect query - cant find mandatory SELECT and WHERE clauses");
 
         var anchors = {
@@ -38,56 +37,105 @@ class QueryParser
         var variables = query
             .match(/\?[a-zA-Z0-9_]+/img)
             .filter(RemoveDublicates)
-            .filter(v=>v.slice(-"Label".length) !== "Label")
-            .map(v=>v.slice(1))
+            .filter(v => v.slice(-"Label".length) !== "Label")
+            .map(v => v.slice(1))
             ;
 
-        return { variables, body:query.trim(), anchors, };
+        return { variables, body: query.trim(), anchors, };
     }
 
-    static InjectLabels(query){
-        var body = query.body
-            
+
+   
+
+    static InjectLabels(query) {
+
         var markedVariables = query.variables
             .sort(Sort)
-            .map(AddVariableQuestionPrefix)
+            .map(AddMark)
             ;
 
-        var beforeSelectedIncluded = body.slice(0, query.anchors.justAfterSelect);
-        var outVariables = [...markedVariables, ...markedVariables.map(AddLabel)].join(" ").trim();
-        var languageFilters = query.variables.map(AddLabelFilterClause).join("\n");
-        var afterWhereAndFuther = body.slice(query.anchors.firstParenthesisAfterWhere + 1);
-            afterWhereAndFuther = this.InjectOptionalRdfsNames(afterWhereAndFuther);
+        var shortener = minify.GenerateShortener(query.variables);
+        var body = this.InjectShortNames(query.body, markedVariables);
+        var trimmed = body.toLowerCase();
+
+
+        var justAfterSelectAnchor = trimmed.indexOf("select") + "select".length;
+        var firstParenthesisAfterWhereAnchor = trimmed.indexOf("{", trimmed.indexOf("where") + "where".length);
+
+
+        var beforeSelectedIncluded = body.slice(0, justAfterSelectAnchor);
+        var outVariables = [
+            ...query.variables.map(shortener).map(AddMark), 
+            ...query.variables.map(shortener).map(AddMark).map(AddLabel), 
+        ].join(" ").trim();
+        var languageFilters = query.variables.map(shortener).map(AddMark).map(AddLabel).map(AddLabelFilterClause).join("\n");
+        var afterWhereAndFuther = body.slice(firstParenthesisAfterWhereAnchor + 1);
+        afterWhereAndFuther = this.InjectOptionalRdfsNames(afterWhereAndFuther);
+        afterWhereAndFuther = this.RemoveLabelServiceDeclaration(afterWhereAndFuther);
 
         var composition = `${beforeSelectedIncluded} ${outVariables} where { \n${languageFilters}\n${afterWhereAndFuther}`
             .replace(/\n/img, " ")
             .replace(/  /img, " ")
             ;
+
         return composition;
     }
 
 
-    static InjectOptionalRdfsNames(afterWhereAndFuther){
+    /** OPTIONAL { ?game wdt:P400 ?platform. } -> OPTIONAL { ?game wdt:P400 ?platform. ?platform rdfs:label ?platformLabel. } */
+    static InjectOptionalRdfsNames(afterWhereAndFuther) {
         var matchesOfOptions = afterWhereAndFuther.match(/OPTIONAL\W[^}]+\}/img);
         var firstMatchIndex = afterWhereAndFuther.indexOf(matchesOfOptions[0]);
-        var lastMatchIndex =  afterWhereAndFuther.lastIndexOf(matchesOfOptions[matchesOfOptions.length-1]);
-        var lastMatchLength = matchesOfOptions[matchesOfOptions.length-1].length;
+        var lastMatchIndex = afterWhereAndFuther.lastIndexOf(matchesOfOptions[matchesOfOptions.length - 1]);
+        var lastMatchLength = matchesOfOptions[matchesOfOptions.length - 1].length;
 
         var optionalList = matchesOfOptions;
-        //OPTIONAL { ?game wdt:P400 ?platform. }
-        //OPTIONAL { ?game wdt:P400 ?platform; rdfs:label ?platformLabel. }
         var optionalsWithRdfsName = optionalList
             .map(option => {
                 var matches = option.match(/(\?\w+)/img);
-                if(!matches) throw new Error("no matches in OPTION - its strange");
+                if (!matches) throw new Error("no matches in OPTION - its strange");
                 var lastMatch = matches.pop();
                 var indexOfLastMatch = option.lastIndexOf(lastMatch);
-                return `${option.slice(0, indexOfLastMatch)} ${lastMatch}; rdfs:label ${lastMatch}${labelAppendix}. }`;
+                return `${option.slice(0, indexOfLastMatch)} ${lastMatch}. ${lastMatch} rdfs:label ${AddLabel(lastMatch)}. }`;
             })
             .join("\n")
             ;
 
-        return `${afterWhereAndFuther.slice(0, firstMatchIndex)} ${optionalsWithRdfsName} ${afterWhereAndFuther.slice(lastMatchIndex+lastMatchLength)}`;
+        return `${afterWhereAndFuther.slice(0, firstMatchIndex)} ${optionalsWithRdfsName} ${afterWhereAndFuther.slice(lastMatchIndex + lastMatchLength)}`;
+    }
+
+    static RemoveLabelServiceDeclaration(afterWhereAndFuther) {
+        var match = afterWhereAndFuther.match(/SERVICE\Wwikibase:label\W[^}]+\}/);
+        var before = afterWhereAndFuther.slice(0, match.index);
+        var after = afterWhereAndFuther.slice(match.index + match[0].length);
+        return before + after;
+    }
+
+    static InjectShortNames(inputString, markedVariables) {
+        var marklessVariables = markedVariables
+            .map(v => v.slice(1))
+            ;
+        var shortener = minify.GenerateShortener(marklessVariables); // delete question mark "?"
+
+        var varMatch = undefined;
+        var cuttedInputString = inputString;
+        var result = [];
+        while ((varMatch = cuttedInputString.match(/\?(\w+)/im)) !== null) {
+            var prevString = cuttedInputString.slice(0, varMatch.index);
+            var fullVar = cuttedInputString
+                .slice(varMatch.index, varMatch.index + varMatch[0].length)
+                .trim()
+                .slice(1) // delete question mark "?"
+                ;
+            var shortVar = shortener(fullVar);
+            result.push([prevString, AddMark(shortVar)]);
+            cuttedInputString = cuttedInputString.slice(varMatch.index + varMatch[0].length);
+        }
+        result.push([cuttedInputString, ""]); //заключительный огрызок оставшийся после последней переменной
+        return result
+            .map(pair => pair[0] + " " + pair[1])
+            .join("")
+            ;
     }
 }
 
